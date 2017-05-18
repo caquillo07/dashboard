3525f5f6ba2f7b2873498a93b01e8e84dd08c861
Hector Mejia <caquillo-0722@hotmail.com>
2017-05-18 02:24:36 -0400

    SIDE BAR IS DONE

--- js/app.js
+++ js/app.js
@@ -1,9 +1,19 @@
-function openNav() {
+function openLeftNav() {
     document.getElementById("sidenav").style.width = "230px";
     document.getElementById("main").style.marginLeft = "230px";
 }
 
-function closeNav() {
+function closeLeftNav() {
     document.getElementById("sidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+}
+
+function openRightNav() {
+    document.getElementById("rightnav").style.width = "365px";
+    document.getElementById("main").style.marginRight = "365px";
+}
+
+function closeRightNav() {
+    document.getElementById("rightnav").style.width = "0";
+    document.getElementById("main").style.marginRight= "0";
 }
